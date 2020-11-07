8ef2144f42b2a2246075e6ce71d08aaa4e11f3be
sudakovandrey <a.sudakov@wear-studio.com>
2020-11-07 23:34:12 +0200

    feat (deeplinking) : setting up deeplinking handler

---
+++ Assets/Scripts/DeeplinkProvider/DeeplinkProvider.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public static class DeeplinkProvider
+{
+	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+	public static void OnApplicationStartHandler()
+		=> Application.deepLinkActivated += DeeplinkingHandler;
+
+	private static void DeeplinkingHandler(string link)
+		=> Debug.Log($"App activated via deeplink {{{link}}}");	
+}
