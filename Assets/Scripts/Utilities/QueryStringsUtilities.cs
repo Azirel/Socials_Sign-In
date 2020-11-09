7680f57680530653d98207eae69f839807c16ee8
sudakovandrey <a.sudakov@wear-studio.com>
2020-11-09 04:34:42 +0200

    feat (utility) : little utility entity to handle query strings

---
+++ Assets/Scripts/Utilities/QueryStringsUtilities.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Specialized;
+using System.Text;
+
+public static class QueryStringsUtilities
+{
+	public static string ConvertQueriesToString(NameValueCollection queries)
+	{
+		var stringBuilder = new StringBuilder();
+		stringBuilder.Append('?');
+		stringBuilder.Append($"{queries.AllKeys[0]}={queries[0]}");
+		queries.Remove(queries.AllKeys[0]);
+		foreach (var key in queries.AllKeys)
+			stringBuilder.Append($"&{key}={queries[key]}");
+		return stringBuilder.ToString();
+	}
+
+	public static NameValueCollection ConvertStringQueries(string queries)
+		=> System.Web.HttpUtility.ParseQueryString(queries);
+}
